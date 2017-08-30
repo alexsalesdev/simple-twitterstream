57b83b9a615688d054f1a885be6c52bb2a6d6fa4
Alex Sales <alexsalesdev@gmail.com>
2017-08-30 11:03:06 +0800

    reactjs working without error but not updating the UI

--- webapp/app/controllers/HomeController.scala
+++ webapp/app/controllers/HomeController.scala
@@ -2,14 +2,14 @@ package controllers
 
 import javax.inject._
 
-import akka.actor.{ActorSystem}
+import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Flow, Sink}
 import play.api.Configuration
+import play.api.libs.json.Json
 import play.api.mvc._
-import services.{Kafka}
+import services.Kafka
 
-import scala.util.parsing.json.JSONObject
 
 @Singleton
 class HomeController @Inject() (cc: ControllerComponents, kafka : Kafka, configuration: Configuration) extends AbstractController(cc) {
@@ -21,19 +21,11 @@ class HomeController @Inject() (cc: ControllerComponents, kafka : Kafka, configu
     Ok(views.html.wordcount(routes.HomeController.wordcountWs().webSocketURL()))
   }
 
-
   def wordcountWs = WebSocket.accept[String, String] { _ =>
     val sink = Sink.foreach[String](println)
 
-    val source = kafka.source("word-count").fold(Map.empty[String, Long]) { (acc, kv) =>
-      val k = kv.key()
-      val v = kv.value()
-      val oldV = acc.get(k).getOrElse(0l)
-      val newV = oldV + v
-
-      acc + (k -> newV)
-    }.map(m => JSONObject(m).toString());
-
+    val source = kafka.source("word-count")
+      .map(f => Json.obj("key" -> f.key(), "val" -> f.value().toString).toString())
     Flow.fromSinkAndSource(sink, source)
   }
 
